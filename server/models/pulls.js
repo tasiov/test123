79580a1d5f50ac59875c96081f501545247f2e59
Orlando Castillo <orlandoc@sas.upenn.edu>
2016-03-02 12:13:40 -0800

    Implement makePullByUser and getPullsByUser methods

--- server/models/pulls.js
+++ server/models/pulls.js
@@ -5,12 +5,14 @@ var _ = require('lodash');
 
 var Pulls = function() {
 	this._pulls = [];
+	this._userId = null;
 } 
 
 Pulls.prototype.makePullByUser = function(pull, callback) {
 	let userHandle = pull.user;
 	pull.closed = Number(pull.closed);
 	delete pull.user;
+	this._pulls.push(pull);
 
 	db.raw(`SELECT internal_id FROM users WHERE login='${userHandle}';`)
   .then((results) => {
@@ -21,7 +23,7 @@ Pulls.prototype.makePullByUser = function(pull, callback) {
 	   _.each(pull,(val,key) => {
 	    pullKeys.push( key + '');
 	    pullVals.push( '"' + val + '"');
-	   })
+	  })
 
 	  return db.raw(`INSERT INTO pulls ( ${pullKeys.join()} )
 	  VALUES (${pullVals.join()})`)
@@ -31,32 +33,24 @@ Pulls.prototype.makePullByUser = function(pull, callback) {
   })
   .catch(console.log);
 }
-  
+ 
 
-
-
-
-
-// Pulls.prototype.getPullsByUser = function (id) {
-//   var self = this;
-//   if (this._issues.length === 0 ||
-//    hoursSinceLastFetch > 1) {
-//     return db.raw(`select i.*, r.language, r.id as repo_id 
-//             from Pulls i 
-//             left join repos r on i.org_name=r.org_name and i.repo_name=r.name 
-//             order by created_at desc;`)
-//             .then((results) => {
-//               results[0].forEach((issue) => {
-//                 issue.labels = JSON.parse(issue.labels);
-//               });
-//               this._issues = results[0];
-//               this._lastUpdateDate = new Date();
-//               return this._issues;
-//             })
-//             .catch(console.log);
-//   } else {
-//     return new Promise((resolve) => resolve(this._issues));
-//   }
-// };
+Pulls.prototype.getPullsByUser = function (userHandle) {
+  var self = this;
+  return db.raw(`SELECT internal_id FROM users WHERE login='${userHandle}';`)
+  .then((results) => {
+  	let userId = results[0][0]['internal_id'];
+  	if (this._pulls.length === 0) {
+  	  return db.raw(`SELECT * FROM pulls WHERE user_id='${userId}';`)
+  	          .then((results) => {
+  	          	this._pulls = results[0]
+  	          	return this._pulls
+  	          })
+  	          .catch(console.log);
+  	} else {
+  	  return new Promise((resolve) => resolve(this._pulls));
+  	}
+  });
+};
 
 module.exports = Pulls;
