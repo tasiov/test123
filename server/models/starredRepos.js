8ff928b3775a4661be725f0aa755dc6aeab55382
Orlando Castillo <orlandoc@sas.upenn.edu>
2016-03-02 17:25:19 -0800

    Implement starredRepos insertion and retreival methods

--- server/models/starredRepos.js
+++ server/models/starredRepos.js
@@ -18,18 +18,33 @@ let dbJoinTableQuery = `users INNER JOIN repos_users
 
 
 
-StarredRepos.prototype.getStarredReposAsync = function(userHandle) {
-  if ( false && this._user.login ) {
+StarredRepos.prototype.getStarredReposAsync = function(userHandle, forceRefresh) {
+  if ( !forceRefresh && this._user.login ) {
   return new Promise((resolve) => resolve(this._starredRepos));
   } else {
   return db.raw(`SELECT repos.name FROM ${dbJoinTableQuery} 
   							WHERE users.login = '${userHandle}'`)
            .then((results) => {
               this._starredRepos = results[0];
-              return results[0];
+              return this._starredRepos;
            }); 
   }
 
+}
+
+StarredRepos.prototype.insertStarredRepoUser = function(gitRepoId, userHandle) {
+	return Promise.all([
+			db.raw(`SELECT internal_id FROM repos WHERE id = ${gitRepoId};`),
+			db.raw(`SELECT internal_id FROM users WHERE login = '${userHandle}';`)
+		]).then((allResults) => {
+			var repoInternalId = allResults[0][0][0]['internal_id'];
+			var userInternalId = allResults[1][0][0]['internal_id'];
+			return db.raw(`INSERT INTO repos_users (repo_id, user_id) 
+							VALUES (${repoInternalId}, ${userInternalId});`)
+							.then( () => {
+								return this.getStarredReposAsync(userHandle, true);
+							});
+		});
 }
 
 module.exports = StarredRepos;
