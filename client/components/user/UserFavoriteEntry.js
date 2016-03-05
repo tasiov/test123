d3a991e0c02ff830196be210b6d0ea44c0b25c05
Edison Huff <b@mboost.net>
2016-03-04 20:12:35 -0800

    Add Toast to Delete function on Favorite Repos Entry`

--- client/components/user/UserFavoriteEntry.js
+++ client/components/user/UserFavoriteEntry.js
@@ -6,23 +6,23 @@ class UserFavoriteList extends React.Component {
   
   constructor(props) {
     super(props);
+    var unFavorite = () => {
+      Materialize.toast(this.props.name + " was removed!", 4000)
+      Favs.deleteFavedRepoFromApi(this.props.id);
+      this.icon = (<i className="material-icons right">loyalty</i>);
+      this.render();
+    }
+    this.icon = (<a><i className="material-icons right" onClick={unFavorite}>loyalty</i></a>)
   }
 
   render() {
 
-    let iconColor = "green";
-    let unFavorite = () => {
-      Materialize.toast(this.props.name + " was removed!", 4000)
-      Favs.deleteFavedRepoFromApi(this.props.id);
-      iconColor = "grey";
-    }
-
-  	
+    
   	return(
       <li className="user-favorite-entry">
         <div className="collapsible-header">
           <span>{this.props.data.name}</span>
-          <a href="#!" className="right" onClick=""><i className="material-icons {iconColor}">loyalty</i></a>
+          {this.icon}
         </div>
         <div className="collapsible-body">
           <p> {this.props.data.org_name}<br/>
